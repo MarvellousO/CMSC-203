d15a2f4ee19b2692c7923d51b8e4faa53d70199c
Marvellous Onajobi <122251904+MarvellousO@users.noreply.github.com>
2025-11-19 23:53:39 -0500

    Add CheckingAccount class with withdrawal logic
    Implement CheckingAccount class extending BankAccount with a withdrawal method that checks balance.

--- CMSC203_Lab1/Lab6/CheckingAccount.java
+++ CMSC203_Lab1/Lab6/CheckingAccount.java
@@ -1,1 +1,25 @@
+public class CheckingAccount extends BankAccount {
 
+	static final double FEE = 0.15;
+
+	public CheckingAccount(String name, double amount){
+		super(name, amount);
+		String accountNumber = super.getAccountNumber()+"-"+"10";
+		super.setAccountNumber(accountNumber);
+	}
+
+	@Override
+	public boolean withdraw(double amount){
+		boolean withdrawEnabled = true;
+		
+		if(amount > super.getBalance() ){
+			return withdrawEnabled;
+		}
+		else
+		{  withdrawEnabled  = false;
+			return withdrawEnabled;
+		}
+
+	}
+
+}

01bfce64cb3415962d1de10baae4ef3dacdf53d1
Marvellous Onajobi <122251904+MarvellousO@users.noreply.github.com>
2025-11-19 23:53:16 -0500

    Update SavingsAccount.java

--- CMSC203_Lab1/Lab6/SavingsAccount.java
+++ CMSC203_Lab1/Lab6/SavingsAccount.java
@@ -1,1 +1,31 @@
+public class SavingsAccount extends BankAccount{
 
+	private double rate = 0.025;
+	private int  savingsNumber = 0;
+	private String accountNumber;
+	
+	public SavingsAccount(String name, double amount)
+	{
+		super(name, amount);
+		this.accountNumber = super.getAccountNumber()+
+							"-" +
+							this.savingsNumber;
+
+	}
+	@Override
+	 public String getAccountNumber(){return accountNumber;}
+	
+	
+	 public void postInterest()
+	 { double oneMonthInterest = getBalance() * (rate/12);
+	 super.deposit(oneMonthInterest);
+	 
+	 }
+
+	 public SavingsAccount(SavingsAccount oldAccount, double amount){
+		 super(oldAccount,amount);
+		 savingsNumber = oldAccount.savingsNumber++;
+		 this.accountNumber = super.getAccountNumber() +"-"+ savingsNumber;
+	 }
+
+}
